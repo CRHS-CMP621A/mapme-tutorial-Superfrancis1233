8bbbb8926d74c8f067780cce66c7717414709dd7
Superfrancis1233 <superfrancis1233@gmail.com>
2023-05-16 14:24:09 -0300

    -

--- GeoLocationAPI/script.js
+++ GeoLocationAPI/script.js
@@ -4,6 +4,17 @@ navigator.geolocation.getCurrentPosition(
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     console.log("https://www.google.com/maps/@" + longitude + "," + latitude);
+
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+
   },
   function() {
     alert("Could not get position.");
