dd004fe1b14e81d718928bc47212e0bc7bc9c37a
Superfrancis1233 <superfrancis1233@gmail.com>
2023-05-16 14:12:19 -0300

    -

--- GeoLocationAPI/script.js
+++ GeoLocationAPI/script.js
@@ -3,7 +3,7 @@ navigator.geolocation.getCurrentPosition(
     // console.log(position);
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    console.log(latitude,longitude);
+    console.log("https://www.google.com/maps/@", longitude,",", latitude);
   },
   function() {
     alert("Could not get position.");
