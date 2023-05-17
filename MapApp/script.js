d96b209f6f7603326a4704cf818b64f0553617b8
Superfrancis1233 <superfrancis1233@gmail.com>
2023-05-17 12:46:19 -0300

    -

--- MapApp/script.js
+++ MapApp/script.js
@@ -11,4 +11,31 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+// Geolocation & Map
+
+  navigator.geolocation.getCurrentPosition(
+  function (position) {
+    // console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log("https://www.google.com/maps/@" + longitude + "," + latitude);
+
+    const coords = [latitude, longitude]
+
+    var map = L.map('map').setView(coords, 13);
+
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker(coords).addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
+
+  },
+  function() {
+    alert("Could not get position.");
+  }
+);
 
