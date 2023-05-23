1f20b885d4447ebb2c7bd8982a41d0f99acb1d52
Superfrancis1233 <superfrancis1233@gmail.com>
2023-05-23 12:56:31 -0300

    -

--- MapApp/script.js
+++ MapApp/script.js
@@ -62,7 +62,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
         // form event listener to check if submitted/completed
         form.addEventListener('submit', function(e){
           e.preventDefault()
-          document.getElementById('.form').reset();
+          document.getElementsByClassName('.running-popup').reset();
         
         })
 
